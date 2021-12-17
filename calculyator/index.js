c9079e935115c1c89e9f314c5fc5fd3292f00e46
RomanVanLenSi <90959647+RomanVanLenSi@users.noreply.github.com>
2021-12-17 20:19:43 +0300

    готов

--- calculyator/index.js
+++ calculyator/index.js
@@ -1,36 +1,14 @@
-const output = document.querySelector('output')
-const div = document.createElement('div')
-div.classList.add('keyboard')
-document.querySelector('.calculator').appendChild(div)
-
-'7 8 9 / c 4 5 6 * ( 1 2 3 - ) 0 . + ='.split(' ')
-.map(symbol =>{
-    div.insertAdjacentHTML('beforeend', `<button value ="${symbol}">${symbol}</button>`)
-})
-document.querySelectorAll('button').forEach(button =>{
-    button.addEventListener('click', function {
-        calc(this.value)
-    })
-})
-document.addEventListener('keydown', event => {
-    if ((event.key).match(/[0-9%\/*\-+\(\)=]|Backspace|Enter/)) calc(event.key)
-})
-function calc(value) {
-    if (value.match(/=|Enter/)) {
-        try {
-            output.textContent = Math.trunc(match.evaluate(output.textContent))
-        
-        } catch {
-            let oldValue = output.textContent
-            let newValue = "недопустиме значение"
-            output.textContent = newValue
-            setTimeout(() => {
-                output.textContent = oldValue
-            }, 1500)
-        }
-    } else if (value === 'C') {
-        output.textContent = ''
-    }else {
-        output.textContent += value
-    }
+function insert (num) {
+    document.form.textview.value = document.form.textview.value + num
+}
+function clean () {
+    document.form.textview.value = ""
+}
+function back() {
+    let exp = document.form.textview.value
+    document.form.textview.value = exp.substring(0, exp.length-1)
+} 
+function equal() {
+    
+    document.form.textview.value = eval(document.form.textview.value)
 }
